70cba517cf9d4da502d814aa05a2810db3964bf3
Jonathan Costa <jonathancosta746@gmail.com>
2022-12-01 14:38:38 -0300

    adicionado sessao tecnologias

--- src/components/Technologies/index.tsx
+++ src/components/Technologies/index.tsx
@@ -1,20 +1,57 @@
-
+import styles from './Technologies.module.css'
+import LogoStyledComponents from '../../assets/styled-components-logo.png'
 
 export const Tecnologies = () => {
   return (
-    <div id="technologies">
-        <h1>Tecnologias</h1>
-            <ul>
-                <li>React.Js</li>
-                <li>TypeScript</li>
-                <li>JavaScript</li>
-                <li>Node.Js</li>
-                <li>Sass</li>
-                <li>Material UI</li>
-                <li>Styled-Components</li>
-                <li>Git</li>
-                <li>MySQL</li>
-            </ul>
+    <div id="technologies" >
+        <h1 className={styles.technologies_h1}>Tecnologias</h1>
+          <div className={styles.technologies}>
+            <div>
+              <i className="devicon-react-original colored"></i>
+              <p>React.Js/Native</p>
+            </div>
+            <div>
+              <i className="devicon-typescript-plain colored"></i>
+              <p>TypeScript</p>
+            </div>
+            <div>
+              <i className="devicon-javascript-plain colored"></i>
+              <p>JavaScript</p>
+            </div>
+            <div>
+              <i className="devicon-nodejs-plain colored"></i>
+              <p>Node.Js</p>
+            </div>
+            <div>
+              <img src={LogoStyledComponents} alt="styled-components-logo" className={styles.technologies_img} />  
+              <p>Styled-Components</p>
+            </div>
+            <div>
+              <i className="devicon-sass-original colored"></i>
+              <p>Sass</p>
+            </div>
+            <div>
+              <i className="devicon-materialui-plain colored"></i>
+              <p>Material UI</p>
+            </div>
+            <div>
+              <i className="devicon-mysql-plain-wordmark"></i>
+              <p>MySQL</p>
+            </div>
+            <div>
+              <i className="devicon-git-plain colored"></i>
+              <p>Git</p>
+            </div>      
+            <div>
+              <i className="devicon-photoshop-line colored"></i>
+              <p>Photoshop</p>
+            </div>      
+            <div>
+              <i className="devicon-figma-plain colored"></i>
+              <p>Figma</p>
+            </div>      
+          </div>
+                        
     </div>
   )
 }
