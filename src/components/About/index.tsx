1497ee9b067cdc520b997ffc8b3534db6325e6f6
Jonathan Costa <jonathancosta746@gmail.com>
2023-03-17 11:26:50 -0300

    atualização about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -13,13 +13,11 @@ export const About = () => {
         <Fade>
           <p>Olá,
 
-            Meu nome é Jonathan e sou um desenvolvedor web especializado e entusiasta de novas tecnologias, com foco atual em desenvolvimento em web com React. Tenho uma paixão por resolver problemas, principalmente aqueles que envolvem tecnologia, e procuro constantemente estar atualizado com as tendências e novas linguagens.
-            <br/>
-            <br/>
-            Estou sempre em busca de desafios e novas oportunidades para crescer como profissional e aprender coisas novas.
-            <br/>
-            <br/>
-            Sou uma pessoa que aprende facilmente, e isso me permitiu adquirir conhecimento sobre novas tecnologias com facilidade, e acredito que minha experiência me permite fornecer soluções personalizadas e de alta qualidade para clientes em diferentes setores.
+          Sou um desenvolvedor web especializado e entusiasta de novas tecnologias, com foco atual em desenvolvimento web com React. Tenho uma paixão por resolver problemas, principalmente aqueles que envolvem tecnologia, e procuro constantemente estar atualizado com as tendências e novas linguagens.
+   
+          Estou sempre em busca de desafios e novas oportunidades para crescer como profissional e aprender coisas novas.
+            
+          Sou uma pessoa que aprende facilmente, e isso me permitiu adquirir conhecimento sobre novas tecnologias com facilidade, e acredito que minha experiência me permite fornecer soluções personalizadas e de alta qualidade para diferentes setores.
           </p>
         </Fade>
       </div>  
