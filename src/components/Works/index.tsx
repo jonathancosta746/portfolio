12499c3a75a063464e1a05e0c90095c8700060f8
Jonathan Costa <jonathancosta746@gmail.com>
2023-07-14 09:42:04 -0300

    ajustado responsividade do componente works

--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -17,7 +17,7 @@ export const Works = () => {
       <div className={styles.work_item}>
         <Fade>
           <div className={styles.work_image}>
-            <img src={workDuoSynchro} alt="Philips" className={styles.work_img}/>
+            <img src={workDuoSynchro} alt="Synchro" className={styles.work_img}/>
           </div>
         </Fade>  
 
@@ -25,7 +25,13 @@ export const Works = () => {
           <div className={styles.work_detail}>
             <h2>Synchro Investimentos</h2>
             <p>- Analista de Dados</p>
-            <p>Atuei na criação de Screener(localizador) para localização e monitoramento automático de ativos financeiros nas características desejadas pelos investidores, onde o script os notificava sobre quais ativos estavam nas características desejadas, fazendo assim com que pudessem ganhar tempo focando apenas nos ativos que estavam na característica esperada.</p>
+          </div>
+        </Fade>
+
+        <Fade>
+          <div className={styles.work_info}>
+            <p>Atuei na criação de Screener(localizador) para localização e monitoramento automático de ativos financeiros nas características desejadas pelos investidores, onde o script os notificava sobre quais ativos estavam nas características desejadas, fazendo assim com que pudessem ganhar tempo focando apenas nos ativos que estavam na característica esperada.
+            </p>
           </div>
         </Fade>
       </div>
@@ -41,10 +47,15 @@ export const Works = () => {
           <div className={styles.work_detail}>
             <h2>Philips</h2>
             <p>- Consultor de Implantação</p>
-            <p>Realizava o treinamento das equipes dos hospitais da rede Opty(HOB) em Brasília durante a migração de sistemas para o sistema Tasy HTML e Tasy Java. Recebendo as críticas e sugestões dos funcionários de cada hospital e as tratando e filtrando para os desenvolvedores tomarem as devidas providências. Também atuei na alimentação de dados do sistema conforme necessário e adaptação da UI para o hospital.
-            </p>
           </div>  
         </Fade> 
+
+        <Fade>
+          <div className={styles.work_info}>
+            <p>Realizava o treinamento das equipes dos hospitais da rede Opty(HOB) em Brasília durante a migração de sistemas para o sistema Tasy HTML e Tasy Java. Recebendo as críticas e sugestões dos funcionários de cada hospital e as tratando e filtrando para os desenvolvedores tomarem as devidas providências. Também atuei na alimentação de dados do sistema conforme necessário e adaptação da UI para o hospital.
+            </p>
+          </div>
+        </Fade>
       </div>  
     </div>
   )
